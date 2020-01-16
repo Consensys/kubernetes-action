1d9dcefdc6dc11ac110f8cd53a2478ecb51f9754
Joaquín Moreira <joaquinscript@gmail.com>
2020-01-16 12:51:14 -0300

    Fix exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,9 @@
 
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
-result=$(kubectl $1)
+
+result="$(kubectl $1)"
+status=$?
 echo ::set-output name=result::$result
+echo "$result"
+if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
