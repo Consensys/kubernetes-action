6582f4aab8c0918fc58ef0b86f9f4c7dbc64ca1d
Bruno Vale <bruno_vale@outlook.com>
2020-03-29 21:42:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
-#!/bin/bash -l
+#!/bin/sh -l
 
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
-command=$1
-if [[ $command == *"kubectl"* ]]; 
-then
-  result="$($command)"
-else
-  result="$(kubectl $command)"
-fi
+
+result="$(kubectl $1)"
+
 status=$?
 echo ::set-output name=result::$result
 echo "$result"
